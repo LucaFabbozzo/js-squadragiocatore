1c54cbcc70039f925c015d076ff76474affe59de
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-23 08:15:31 -0500

    advanced version

--- js/script.js
+++ js/script.js
@@ -1,36 +1,41 @@
 //come poter creare un array multidimensionale e a lavorare nel dom con liste dinamiche
 
 const data = [];
+//shortcut per scrivere queryselector soltanto una volta
+const el = document.querySelector.bind(document);
 
 //prendiamo il bottone
-document.querySelector('button').addEventListener('click', function() {
+el('[name="player"]').addEventListener('keypress', function(event) {
+  //azionare il keypress solamente sull'enter, 
+  //se keycode è il numero 13 che corrisponde a enter fermati qui.
+  if (event.keyCode != 13) return;
   // andiamo a prendere i valori dei due input con name team e player
-  const team = document.querySelector('[name="team"]').value;
-  const player = document.querySelector('[name="player"]').value;
+  const team = el('[name="team"]').value;
+  const player = el('[name="player"]').value;
   console.log(team, player);
   //adesso dobbiamo inserire il player all'interno di team pero' in un sottonodo di data.
   // controlliamo se esiste data di team, se non esiste dobbiamo dire che data di team è a sua volta un'array
   if (!data[team]) {
     data[team] = [];
     //adesso ci servono un po' di elementi html all'interno della pagina quindi ci servira una lista
-    const list = document .createElement('ul');
+    const list = document.createElement('ul');
     list.classList.add('list-group');
     //set attribute permette di aggiungere altre informazioni all'interno di un elemento, in questo caso va ad ignettare l'attributo data-team = "team"
     list.setAttribute('data-team', team);
 
     //creiamo un titolo, il titolo sara il nome della squadra
-    const title = document .createElement('h2');
+    const title = document.createElement('h2');
     title.classList.add('text-center');
     title.innerText = team;
 
     //ci serve anche un div dove andremo ad appendere sia il title sia la lista
-    const div = document .createElement('h2');
+    const div = document.createElement('h2');
     div.classList.add('mb-4');
     div.append(title);
     div.append(list);
 
     //dobbiamo aggiungere il div all'interno del main
-    document.querySelector('main').append(div);
+    el('main').append(div);
   }
 
   data[team].push(player)
@@ -41,5 +46,14 @@ document.querySelector('button').addEventListener('click', function() {
   item.innerText = player;
 
   //adesso dobbiamo appendere questo item all'interno della nostra lista che abbiamo creato
-  document.querySelector('[data-team="' + team + '"]').append(item);
+  el('[data-team="' + team + '"]').append(item);
+
+  //svuotiamo gli input
+  el('[name="team"]').value = '';
+  el('[name="player"]').value = '';
+
+  //riposizionamo il focus su team
+
+  el('[name="team"]').focus();
+
 })
